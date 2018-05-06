fb3322443e4ad1dbb28caaa87b8663652e195b5c
Stars <bestleonid2000@gmail.com>
2018-05-06 04:44:37 -0500

    Initial Commit

---
+++ streamingSongTests/streamingSongTests.swift
@@ -0,0 +1,36 @@
+//
+//  streamingSongTests.swift
+//  streamingSongTests
+//
+//  Created by Stars on 5/6/18.
+//  Copyright © 2018 Stars. All rights reserved.
+//
+
+import XCTest
+@testable import streamingSong
+
+class streamingSongTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
